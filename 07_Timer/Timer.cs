e70aee90263cfa0d660eb26bc5fa92ae01dc1e8b
kaans <kaans@DESKTOP-U4CBDT5>
2016-09-20 09:24:18 +0200

    Add project files.

---
+++ 07_Timer/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _07_Timer
+{
+    internal class Timer
+    {
+        public int Seconds = 0;
+        
+        public override string ToString()
+        {
+            var time = TimeSpan.FromSeconds(Seconds);
+            return time.ToString(@"hh\:mm\:ss");
+        }
+
+        internal string Padded(int input)
+        {
+            return input.ToString("00");
+        }
+    }
+}
